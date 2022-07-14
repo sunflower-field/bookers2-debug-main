f8a0bd31c74b5aaf05d301da15f589920c4f3d54
sunflower-field <himartmusume@gmail.com>
2022-07-14 14:33:40 +0000

    [Add]フォロー機能

---
+++ app/models/relationship.rb
@@ -0,0 +1,5 @@
+class Relationship <
+   belongs_to :following, class_name: "User"
+   belongs_to :follower, class_name: "User"
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,12 @@ class User < ApplicationRecord
  has_one_attached :profile_image
 
  has_many :book_comments, dependent: :destroy
+ 
+  # foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
+  has_many :relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
+  # @user.booksのように、@user.yyyで、
+  # そのユーザがフォローしている人orフォローされている人の一覧を出したい
+  has_many :yyy, through: :xxx, source: :zzz
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
